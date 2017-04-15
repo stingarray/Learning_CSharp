608cf058b3663d4884eddbdf5c61b15e41a53517
Carrah Osborn <carraheosborn@hotmail.com>
2017-04-15 18:25:09 -0500

    Adds for loop project.

---
+++ ForLoop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myArray = new string[5];
+
+            myArray[0] = "Tiger";
+            Console.WriteLine(myArray[0]);
+
+            myArray[1] = "Burmese Python";
+            Console.WriteLine(myArray[1]);
+
+            myArray[2] = "Bear";
+            Console.WriteLine(myArray[2]);
+
+            myArray[3] = "Flamingo";
+            Console.WriteLine(myArray[3]);
+
+            myArray[4] = "Ostrich";
+            Console.WriteLine(myArray[4]);
+
+            for(int i = 0; i < myArray.Length; i++)
+
+            {
+                Console.WriteLine("myArray index " + i + "= " + myArray[i]);
+               
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
